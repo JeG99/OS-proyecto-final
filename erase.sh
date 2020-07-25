c53d494622e07197247b94667b11099e7dd1e0ec
adrianafdz <68704785+adrianafdz@users.noreply.github.com>
2020-07-25 11:21:57 -0500

    Update erase.sh
    Todos los casos de flags completados.

--- erase.sh
+++ erase.sh
@@ -7,6 +7,13 @@
 #################################################
 
 # Validar si existe el directorio
+direct=`ls -la | egrep ".Kuka$" `
+if [ $? -ne 0 ] # si no existe, lo creo
+then
+    mkdir 2>/dev/null ~/.Kuka
+fi
+
+# Validar si existe el directorio
 direct=`ls -la | egrep " .Kuka" `
 if [ $? -ne 0 ] # si no existe, lo creo
 then
@@ -18,45 +25,81 @@ if [ $# -gt 0 -a $# -lt 3 ]
 then
 
     case $1 in
-        -S)
-    	    # Muestra archivos en el directorio
-            ls -l ~/.Kuka | egrep -iv total | tr -s '[ ]' '[#*]' | tr -s '[     ]' '[#*]' | cut -f9 -d#
-            ;;
+                -S) # Muestra archivos en el directorio
+                    # Verificar que no haya más argumentos
+                    if [ $# -gt 1 ]
+                    then
+                        echo "\nERROR: No arguments required.\a"
+                    else
+                        ls -l ~/.Kuka | egrep -iv total | tr -s '[ ]' '[#*]' | tr -s '[ ]' '[#*]' | cut -f9 -d#
+                    fi
+                    ;;
 
-        -D)
-            # Borra todos los archivos
-            Lista=`ls ~/.Kuka`
-            echo "\nAre you sure you want to delete all files? (Y/N): "
-            read YN
 
-            if [ $YN = "Y" ]
-            then
-                for arch in $Lista
-                do
-                    rm ~/.Kuka/$arch
-                done
-            fi
-            ;;
+                -D) # Borra todos los archivos del directorio Kuka
+                    # Verificar que no haya más argumentos
+                    if [ $# -gt 1 ]
+                    then
+                        echo "\nERROR: No arguments required.\a"
+                    else
+                        # Borra todos los archivos
+                        Lista=`ls ~/.Kuka`
 
-        -I)
-            # Para cada archivo, pregunta si lo quiere borrar
-            Lista=`ls ~/.Kuka`
-            echo $Lista
-            for arch in $Lista
-            do
-                echo "\nDo you really want to delete the file \"$arch\"? (Y/N): "
-                read YN
-                if [ $YN = "Y" ]
-                then
-                    rm  ~/.Kuka/$arch
-                fi
-            done
-            ;;
+                        echo "Are you sure you want to delete all files?"
+                        read YN
+                        if [ $YN = "Y" ]
+                        then
+                            for arch in $Lista
+                            do
+                                rm 2>/dev/null ~/.Kuka/$arch
+                            done
+                        fi
+                    fi
+                    ;;
 
-        *)
-            echo "\nError: The flag used is not valid in this script !!!\a"
-            ;;
-    esac
+                -I) # Para cada archivo, pregunta si lo quiere borrar
+                    # Verificar que no haya más argumentos
+                    if [ $# -gt 1 ]
+                    then
+                        echo "\nERROR: No arguments required.\a"
+                    else
+                        Lista=`ls ~/.Kuka`
+
+                        # Procesar la lista
+                        for arch in $Lista
+                        do
+                            # Verifica con el usuario
+                            echo "\nDo you really want to delete the file \"$arch\"? (Y/N): "
+                            read YN
+                            if [ $YN = "Y" ]
+                            then
+                                rm  ~/.Kuka/$arch
+                            fi
+                        done
+                    fi
+                    ;;
+
+
+                -M) # Mueve un archivo a la carpeta Kuka
+                    # Verificar que tenga un segundo argumento
+                    if [ $# -eq 2 ]
+                    then
+                        # Checar que exista el archivo
+                        if test -f $2
+                        then
+                                mv $2 ~/.Kuka/.
+                        else
+                                echo "\nError: the file \"$2\" does not exist.\a"
+                        fi
+                    else
+                        echo "\nError: The script must receive the name of the file to be deleted\a"
+                    fi
+                    ;;
+
+                *)
+                    echo "\nError: The flag used is not valid in this script !!!\a"
+                    ;;
+        esac
 
 else
     echo "\nERROR: Wrong number of arguments.\a"
